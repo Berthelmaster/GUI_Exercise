f6bbb34f6d79644ce6935ce46e51983dc073f43a
Nichlaes <nichlaes.soerensen@gmail.com>
2019-03-12 11:38:33 +0100

    folders in solutionen

---
+++ SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/AddDebtorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class AddDebtorViewModel
+{
+	public AddDebtorViewModel()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
