c518aa62e8380b9181f847e38bf4b51ca05f5a9a
Alexander Bjerre <201305878@post.au.dk>
2019-03-12 11:30:24 +0100

    Add viewmodel example

---
+++ BMICalculator/BMICalculator/Model/BMIModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BMICalculator.Model
+{
+    class BMIModel
+    {
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+        public double CalculateBMI()
+        {
+            double bmi = Weight / (Height * Height);
+            return bmi;
+        }
+
+    }
+}
---
+++ BMICalculator/BMICalculator/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMICalculator.Model;
+
+namespace BMICalculator.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public BMIViewModel BmiViewModel
+        {
+            get { return new BMIViewModel(); }
+        }
+    }
+}
