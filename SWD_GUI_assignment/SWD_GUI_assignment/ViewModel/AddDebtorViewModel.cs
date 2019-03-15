2c91c1096667f576d8b0bb1e975801b152bdaa97
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-03-15 10:07:17 +0100

    AddDebtorViewModel PropertyChangedEventHandler implemented

--- SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/AddDebtorViewModel.cs
+++ SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/AddDebtorViewModel.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using SWD_GUI_assignment.Annotations;
 
 namespace SWD_GUI_assignment.ViewModel
 {
-    class AddDebtorViewModel
+    class AddDebtorViewModel: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
 
     }
 
