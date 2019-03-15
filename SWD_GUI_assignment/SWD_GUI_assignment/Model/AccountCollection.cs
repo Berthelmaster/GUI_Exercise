6f8c8ba5dc104d8771c97b4a2e4f2e299c287cf3
Alexander Bjerre <201305878@post.au.dk>
2019-03-15 14:06:06 +0100

    add AccountCollection

---
+++ SWD_GUI_assignment/SWD_GUI_assignment/Model/AccountCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SWD_GUI_assignment.Model
+{
+    public class AccountCollection
+    {
+       // Plz fix Chlaes :)
+       // List<AccountModel> Accounts = new List<AccountModel>();
+
+
+    }
+}
