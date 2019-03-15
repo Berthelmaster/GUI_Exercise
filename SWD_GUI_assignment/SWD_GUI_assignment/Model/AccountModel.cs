d1ec16dba95314648e613908b03926b665b3ec4e
Alexander Bjerre <201305878@post.au.dk>
2019-03-15 09:42:33 +0100

    add accountmodel

---
+++ SWD_GUI_assignment/SWD_GUI_assignment/Model/AccountModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD_GUI_assignment.Model
+{
+    class AccountModel
+    {
+        private string _name;
+        private double _balance;
+
+        public AccountModel(string name, double balance = 0)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public double Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+    }
+
+} 
