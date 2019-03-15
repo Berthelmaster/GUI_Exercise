66114236e2417c4172bdb0ff99ff3a6fe878b70d
Jannik <Jannik.lading@hotmail.dk>
2019-03-15 12:19:11 +0100

    DebtorOverview updated

--- SWD_GUI_assignment/SWD_GUI_assignment/View/DebtorOverview.xaml.cs
+++ SWD_GUI_assignment/SWD_GUI_assignment/View/DebtorOverview.xaml.cs
@@ -12,17 +12,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SWD_GUI_assignment.Model;
 
-namespace Debtoroverview
+namespace SWD_GUI_assignment
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DebtorOverview : Window
     {
-        public MainWindow()
+        private AccountModel _account;
+
+        public DebtorOverview(AccountModel account)
         {
+            _account = account;
+            Name.Content = _account.Name; 
             InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            _account.ChangeBalance(double.Parse(Value.Text));
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
