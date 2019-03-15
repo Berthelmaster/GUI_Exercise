0342a4c13472822ac1db20e03c3c80864aa0bfe0
Berthelmaster <berthel0802@gmail.com>
2019-03-15 09:32:04 +0100

    Last changes to ViewModelLocator

--- SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/ViewModelLocator.cs
+++ SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/ViewModelLocator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BMICalculator.Model;
+using SWD_GUI_assignment.Model;
 using SWD_GUI_assignment.ViewModel;
 
 namespace BMICalculator.ViewModel
