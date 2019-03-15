241bf9081c11374bb10bdf61d71d6f01ff816f41
Nichlaes <nichlaes.soerensen@gmail.com>
2019-03-15 09:47:59 +0100

    Implemented MainWindow and started Viewmodel

--- SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/MainWindowViewModel.cs
+++ SWD_GUI_assignment/SWD_GUI_assignment/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 
 namespace SWD_GUI_assignment.ViewModel
 {
-	class MainWindowViewModel
+	public class MainWindowViewModel: INotifyPropertyChanged
 	{
+        ObservableCollection<AccountModel> debtors;
 
-	}
+        public MainWindowViewModel()
+        {
+            debtors = new ObservableCollection<AccountModel>();
+            debtors.Add(new Agent("001", "Nina", "Assassination", "UpperVolta"));
+            debtors.Add(new Agent("007", "James Bond", "Martinis", "North Korea"));
+            CurrentDebtor = debtors[0];
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
